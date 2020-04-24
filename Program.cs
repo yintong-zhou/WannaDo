6193e26bf5b559f5717c36219edf5561966d726e
Yintong <zhouyintong96@gmail.com>
2020-04-24 19:10:27 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+
+namespace WannaDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
